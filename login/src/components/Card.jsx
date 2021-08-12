15fa627ab0d610f1ee51b166420351cea9213e0a
Ramos Felipe <felipeofor@gmail.com>
2021-08-12 13:13:28 -0300

    version 1.5

--- login/src/components/Card.jsx
+++ login/src/components/Card.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-function Card ({ avatar, name, id }) {
+function Card ({ avatar, name, id, key }) {
 
     return(
-        <div className="card">
+        <div className="card" alt={key}>
             <img src={avatar}/>
             <h3>{name}</h3>
             <p>{id}</p>
--- login/src/components/CardConteiner.jsx
+++ login/src/components/CardConteiner.jsx
@@ -1,20 +1,21 @@
-import React, { Link } from 'react';
+import React, { useState } from 'react';
 import Card from './Card';
 
 function Toast() {
 
-        // const [usuario, setUsuarios] = useState()
+        const [user, setUsuarios] = useState()
 
-    // // API CALL  - Llamado a un archivo .JSON local
-    // // Creo una función asincrónica
-    // const getProducts = async () => {
-    //     // Espero a que la data se fetchee
-    //     const data = await fetch('https://api.github.com/search/users?q=${usuario}')
-    //     // Aplico el método JSON() para extraer la respuesta a la petición
-    //     const responseData = await data.json()
-    //     // Vemos qué llegó
-    //     setUsuarios(responseData)
-    // }
+    // API CALL  - Llamado a un archivo .JSON local
+    // Creo una función asincrónica
+    const getProducts = async () => {
+        // Espero a que la data se fetchee
+        const data = await fetch('https://api.github.com/search/users?q=felipe')
+        // Aplico el método JSON() para extraer la respuesta a la petición
+        const responseData = await data.json()
+        // Vemos qué llegó
+        console.log(responseData);
+        setUsuarios(responseData)
+    }
 
     return(
         <div className="CardConteiner">
