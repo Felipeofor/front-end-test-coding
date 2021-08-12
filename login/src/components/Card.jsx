a1b5db12f5b5b22227914b1e6f594ba223461fa6
Ramos Felipe <felipeofor@gmail.com>
2021-08-12 10:45:56 -0300

    version 1.1

--- login/src/components/Card.jsx
+++ login/src/components/Card.jsx
@@ -3,25 +3,25 @@ import { searchUsuario } from '../api';
 
 function Card () {
 
-    onst [usuario, setUsuarios] = useState()
+    // const [usuario, setUsuarios] = useState()
 
-    // API CALL  - Llamado a un archivo .JSON local
-    // Creo una función asincrónica
-    const getProducts = async () => {
-        // Espero a que la data se fetchee
-        const data = await fetch('https://api.github.com/search/users?q=${usuario}')
-        // Aplico el método JSON() para extraer la respuesta a la petición
-        const responseData = await data.json()
-        // Vemos qué llegó
-        setUsuarios(responseData)
-    }
+    // // API CALL  - Llamado a un archivo .JSON local
+    // // Creo una función asincrónica
+    // const getProducts = async () => {
+    //     // Espero a que la data se fetchee
+    //     const data = await fetch('https://api.github.com/search/users?q=${usuario}')
+    //     // Aplico el método JSON() para extraer la respuesta a la petición
+    //     const responseData = await data.json()
+    //     // Vemos qué llegó
+    //     setUsuarios(responseData)
+    // }
 
     return(
         <div className='containerCard card mx-1 my-1' >
-                <Link to={`/user.login/${user}`} className='linkCard col-12'>
+                {/* <Link to={`/user.login/${user}`} className='linkCard col-12'>
                 <div className='imageCard col-12' style={{backgroundImage:`url(${avatar_url})`, width: '80%'}}></div>
                 <h3 key={user.id} >{user.login}</h3>
-                </Link>
+                </Link> */}
             </div>
     );
 }
